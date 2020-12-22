de3ca757229da6e488422804f8c546c184bdca54
Michael Jeffrey <michaeldjeffrey@gmail.com>
2020-12-21 23:56:47 -0700

    Trevors was much easier to read with A,B

--- apps/day22/src/day22.erl
+++ apps/day22/src/day22.erl
@@ -16,49 +16,47 @@ recursive_game([], Cards, _Round, _Game, _) ->
 recursive_game(Cards, [], _Round, _Game, _) ->
     %% io:format("Player 1 wins Game ~p after ~p rounds with score of ~p~n", [_Game, _Round, score(Cards)]),
     {player1, score(Cards)};
-recursive_game([N1 | R1]=P1, [N2 | R2]=P2, Round, Game, Seen) ->
+recursive_game([A | T1]=P1, [B | T2]=P2, Round, Game, Seen) ->
     %% io:format("-- Round ~p (Game ~p) --~n", [Round, Game]),
     %% io:format("Player 1 deck: ~p~nPlayer 2 deck: ~p~n", [P1, P2]),
-    %% io:format("Player 1 plays: ~p~nPlayer 2 plays: ~p~n", [N1, N2]),
+    %% io:format("Player 1 plays: ~p~nPlayer 2 plays: ~p~n", [A, B]),
     Seen1 = sets:add_element({P1, P2}, Seen),
     case the_rules(P1, P2, Seen) of
         game_seen -> recursive_game(P1, [], Round + 1, Game, Seen1);
         recurse ->
             %% io:format("Playing a sub-game to determint eh winner...~n~n"),
-            SubDeck1 = lists:sublist(R1, N1),
-            SubDeck2 = lists:sublist(R2, N2),
-            case new_game(N1, N2, SubDeck1, SubDeck2, Game + 1) of
-                {player1, Cards} -> recursive_game(R1 ++ Cards, R2, Round + 1, Game, Seen1);
-                {player2, Cards} -> recursive_game(R1, R2 ++ Cards, Round + 1, Game, Seen1)
+            SubDeck1 = lists:sublist(T1, A),
+            SubDeck2 = lists:sublist(T2, B),
+            case new_game(A, B, SubDeck1, SubDeck2, Game + 1) of
+                {player1, Cards} -> recursive_game(T1 ++ Cards, T2, Round + 1, Game, Seen1);
+                {player2, Cards} -> recursive_game(T1, T2 ++ Cards, Round + 1, Game, Seen1)
             end;
         {player1, Cards} ->
             %% io:format("Player 1 wins round ~p of game ~p!~n~n", [Round, Game]),
-            recursive_game(R1 ++ Cards, R2, Round + 1, Game, Seen1);
+            recursive_game(T1 ++ Cards, T2, Round + 1, Game, Seen1);
         {player2, Cards} ->
             %% io:format("Player 2 wins round ~p of game ~p!~n~n", [Round, Game]),
-            recursive_game(R1, R2 ++ Cards, Round + 1, Game, Seen1)
+            recursive_game(T1, T2 ++ Cards, Round + 1, Game, Seen1)
     end.
 
-the_rules([H1|T1]=One, [H2|T2]=Two, Seen) ->
+the_rules(One, Two, Seen) ->
     case sets:is_element({One, Two}, Seen) of
-        true ->
-            game_seen;
-        false ->
-            case {H1 =< length(T1), H2 =< length(T2)} of
-                {true, true} ->
-                    recurse;
-                _ ->
-                    compare(H1, H2)
-            end
+        true -> game_seen;
+        false -> the_rules(One, Two, unseen)
+    end;
+the_rules(One, Two, unseen) ->
+    case can_recurse(One) andalso can_recurse(Two) of
+        true -> recurse;
+        false -> compare(One, Two)
     end.
 
-new_game(C1, C2, Deck1, Deck2, Game) ->
+can_recurse([H | T]) -> H =< length(T).
+
+new_game(A, B, Deck1, Deck2, Game) ->
     %% io:format("~n=== Game ~p ===~n~n", [Game]),
     case recursive_game(Deck1, Deck2, 0, Game, sets:new()) of
-        {player1, _Score} ->
-            {player1, [C1, C2]};
-        {player2, _Score} ->
-            {player2, [C2, C1]}
+        {player1, _Score} -> {player1, [A, B]};
+        {player2, _Score} -> {player2, [B, A]}
     end.
 
 game([], Cards, _Round) ->
@@ -67,17 +65,17 @@ game([], Cards, _Round) ->
 game(Cards, [], _Round) ->
     %% io:format("Player 1 wins after ~p rounds~n", [_Round]),
     score(Cards);
-game([N1 | R1]=_P1, [N2 | R2]=_P2, Round) ->
+game([A | T1]=_P1, [B | T2]=_P2, Round) ->
     %% io:format("-- Round ~p --~n", [Round]),
     %% io:format("Player 1 deck: ~p~nPlayer 2 deck: ~p~n", [_P1, _P2]),
-    %% io:format("Player 1 plays: ~p~nPlayer 2 plays: ~p~n", [N1, N2]),
-    case compare(N1, N2) of
+    %% io:format("Player 1 plays: ~p~nPlayer 2 plays: ~p~n", [A, B]),
+    case compare(A, B) of
         {player1, Cards} ->
             %% io:format("Player 1 wins the round!~n"),
-            game(R1 ++ Cards, R2, Round + 1);
+            game(T1 ++ Cards, T2, Round + 1);
         {player2, Cards} ->
             %% io:format("Player 2 wins the round!~n"),
-            game(R1, R2 ++ Cards, Round + 1)
+            game(T1, T2 ++ Cards, Round + 1)
     end.
 
 score(Cards) ->
@@ -86,6 +84,8 @@ score(Cards) ->
     Combined = lists:map(fun ({X,Y}) -> X*Y end, Indexed),
     lists:sum(Combined).
 
+compare([A | _], [B | _]) ->
+    compare(A, B);
 compare(One, Two) ->
     case One > Two of
         true -> {player1, [One, Two]};
